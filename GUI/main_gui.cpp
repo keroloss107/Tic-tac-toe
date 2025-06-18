9e49c15cce30357e09f3dea9d319111e75041969
Keroloss Arwehla <kerolossarwehla159@gmail.com>
2025-06-18 17:13:21 +0300

    The entry point for the graphical user interface

--- GUI/main_gui.cpp
+++ GUI/main_gui.cpp
@@ -1,9 +1,15 @@
 #include "LoginWindow.h"
 #include <QApplication>
+#include "DatabaseManager.h"
 
 int main(int argc, char* argv[])
-{
+{    
     QApplication app(argc, argv);
+    if (!DatabaseManager::instance().connect()) {
+        qWarning() << "Failed to connect to database!";
+        return -1;
+    }
+
     LoginWindow window;
     window.show();
     return app.exec();
