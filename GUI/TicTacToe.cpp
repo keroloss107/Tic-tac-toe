0b9c1f4043bbc19d62d11b50a8aff27f4eb32879
Keroloss Arwehla <kerolossarwehla159@gmail.com>
2025-06-18 17:16:31 +0300

    Tic tac toe game

--- GUI/TicTacToe.cpp
+++ GUI/TicTacToe.cpp
@@ -4,6 +4,7 @@
 #include "GameModeWindow.h"
 #include "MainMenuWindow.h"
 #include "LoginWindow.h"
+#include "DatabaseManager.h"
 
 TicTacToeWindow::TicTacToeWindow(QWidget* parent)
     : QMainWindow(parent)
@@ -78,48 +79,76 @@ void TicTacToeWindow::updateBoard()
         for (int col = 0; col < 3; ++col) {
             char mark = board.getCell(row, col);
             int index = row * 3 + col;
-            if (mark == 'X' || mark == 'O') {
-                boardButtons_[index]->setText(QString(mark));
+
+            if (mark == 'X') {
+                boardButtons_[index]->setText("X");
+                boardButtons_[index]->setStyleSheet(
+                    "color: #ff4d6d; background-color: #1f4068; border: 2px solid #00d4ff; "
+                    "font-size: 40px; font-weight: bold; border-radius: 12px;");
+            }
+            else if (mark == 'O') {
+                boardButtons_[index]->setText("O");
+                boardButtons_[index]->setStyleSheet(
+                    "color: #00d4ff; background-color: #1f4068; border: 2px solid #00d4ff; "
+                    "font-size: 40px; font-weight: bold; border-radius: 12px;");
             }
             else {
                 boardButtons_[index]->setText("");
+                boardButtons_[index]->setStyleSheet(
+                    "color: white; background-color: #1f4068; border: 2px solid #00d4ff; "
+                    "font-size: 40px; font-weight: bold; border-radius: 12px;");
             }
         }
     }
 }
+
 
 void TicTacToeWindow::checkGameOver()
 {
     int result = game_.getBoard().evaluate();
     bool movesLeft = game_.getBoard().isMovesLeft();
 
+    extern QString username;
+
+    QString resultText;
+    QString opponentName = (username == QString::fromStdString(game_.getPlayer1Name()))
+        ? QString::fromStdString(game_.getPlayer2Name())
+        : QString::fromStdString(game_.getPlayer1Name());
+
     if ((result == 10 && currentPlayerSymbol_ == game_.getPlayer2Symbol()) ||
         (result == -10 && currentPlayerSymbol_ == game_.getPlayer1Symbol()))
     {
-        QString winner = QString::fromStdString(currentPlayerName_);
+        if (currentPlayerName_ == game_.getPlayer1Name()) {
+            resultText = username + " wins";
+        }
+        else {
+            resultText = username + " lost";
+        }
+
+        DatabaseManager::instance().saveGameResult(username, opponentName, resultText);
+
         QMessageBox::StandardButton reply;
-        reply = QMessageBox::question(this, "Game Over", winner + " wins!\n\nPlay Again?",
+        reply = QMessageBox::question(this, "Game Over", resultText + "\n\nPlay Again?",
             QMessageBox::Yes | QMessageBox::No);
 
         if (reply == QMessageBox::Yes) {
             this->close();
-            // Open GameModeWindow again
             GameModeWindow* gameMode = new GameModeWindow();
             gameMode->show();
         }
         else {
             this->close();
-            // Open MainMenuWindow again
-            
-			extern QString username; // Assuming username is defined globally
-
             MainMenuWindow* mainMenu = new MainMenuWindow(username);
             mainMenu->show();
         }
+
         gameOver_ = true;
     }
     else if (!movesLeft)
     {
+        resultText = "Draw";
+        DatabaseManager::instance().saveGameResult(username, opponentName, resultText);
+
         QMessageBox::StandardButton reply;
         reply = QMessageBox::question(this, "Game Over", "It's a Draw!\n\nPlay Again?",
             QMessageBox::Yes | QMessageBox::No);
@@ -131,11 +160,10 @@ void TicTacToeWindow::checkGameOver()
         }
         else {
             this->close();
-            
-			extern QString username; // Assuming username is defined globally
             MainMenuWindow* mainMenu = new MainMenuWindow(username);
             mainMenu->show();
         }
+
         gameOver_ = true;
     }
 }
@@ -171,4 +199,4 @@ void TicTacToeWindow::updateStatusLabel()
 {
     QString label = QString::fromStdString(currentPlayerName_) + "'s Turn (" + currentPlayerSymbol_ + ")";
     ui->titleLabel->setText(label);
-}
+}            
